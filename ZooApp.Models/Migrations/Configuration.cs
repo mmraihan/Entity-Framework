51f7ca8bb1ebd167a2d53bf0d917313f32d75c8d
mmraihan <mm_raihan@live.com>
2021-09-17 15:03:08 +0600

    Add project files.

---
+++ ZooApp.Models/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+﻿namespace ZooApp.Models.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ZooApp.Models.ZooContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ZooApp.Models.ZooContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+            context.Animals.Add(new Animal()
+            {
+                Food = "Fish",
+                Name = "Dolphin",
+                Origin = "Ocean",
+                Quantity = 5
+
+            });
+            context.SaveChanges();
+        }
+    }
+}
