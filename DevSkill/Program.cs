e1aaf5c5108075096f5a30f395f1fa80f0c92a23
mmraihan <mm_raihan@live.com>
2021-09-19 02:42:36 +0600

    One to Many Data Insert

--- DevSkill/Program.cs
+++ DevSkill/Program.cs
@@ -1,6 +1,7 @@
 ﻿using DevSkill.DbContexts;
 using DevSkill.Entities;
 using System;
+using System.Collections.Generic;
 
 namespace DevSkill
 {
@@ -9,15 +10,36 @@ namespace DevSkill
         static void Main(string[] args)
         {
             TrainingContext context = new TrainingContext();
-            Student student = new Student()
-            {
-                Name = "Raihan",
-                DateOfBirth = new DateTime(1996, 1, 1),
-                CGPA = 3.27,
-            };
+            #region Data Insert
+            //Student student = new Student()
+            //{
+            //    Name = "Raihan",
+            //    DateOfBirth = new DateTime(1996, 1, 1),
+            //    CGPA = 3.27,
+            //};
 
-            context.students.Add(student);
+            //context.students.Add(student);
+            //context.SaveChanges();
+            #endregion
+
+
+            #region One to Many Relationship
+            Course course = new Course();
+            course.Title = "Advance C#";
+            course.Fees = 8500;
+          
+            course.Topics = new List<Topic>();
+            course.Topics.Add(new Topic { Name = "Delegates" });
+            course.Topics.Add(new Topic { Name = "LINQ" });
+
+            context.Course.Add(course);
             context.SaveChanges();
+
+        
+
+
+
+            #endregion
 
 
 
