1ab8b646204efefdf77433a676df06ef62ae1997
mmraihan <mm_raihan@live.com>
2021-09-19 03:16:41 +0600

    Retrieving Data (One to Many RelationShip)

--- DevSkill/Program.cs
+++ DevSkill/Program.cs
@@ -1,7 +1,9 @@
 ﻿using DevSkill.DbContexts;
 using DevSkill.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DevSkill
 {
@@ -24,24 +26,34 @@ namespace DevSkill
 
 
             #region One to Many Relationship
-            Course course = new Course();
-            course.Title = "Advance C#";
-            course.Fees = 8500;
-          
-            course.Topics = new List<Topic>();
-            course.Topics.Add(new Topic { Name = "Delegates" });
-            course.Topics.Add(new Topic { Name = "LINQ" });
+            //Course course = new Course();
+            //course.Title = "Advance C#";
+            //course.Fees = 8500;
 
-            context.Course.Add(course);
-            context.SaveChanges();
+            //course.Topics = new List<Topic>();
+            //course.Topics.Add(new Topic { Name = "Delegates" });
+            //course.Topics.Add(new Topic { Name = "LINQ" });
 
-        
-
-
+            //context.Course.Add(course);
+            //context.SaveChanges();
 
             #endregion
 
+            #region Retrieving Data One to Many
 
+            var course=context.Course.Where(x => x.Id == 2)
+                .Include("Topics")
+                .FirstOrDefault();
+
+            Console.WriteLine($"Course Title: {course.Title}");
+            Console.WriteLine("--------Topics:--------------");
+            foreach (var item in course.Topics)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadKey();
+
+            #endregion
 
         }
     }
